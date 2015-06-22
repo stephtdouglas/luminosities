9e3e96854ca6f16c39fdb2e8b6b4e956fe3b0ccb
stephtdouglas <stephanietdouglas@gmail.com>
2015-06-22 15:21:06 -0400

    constants used in other places in the code

---
+++ luminosities/constants.py
@@ -0,0 +1,10 @@
+
+LBOL_SUN = 3.844e33 # in erg/s (from Bahcall et al 1995, Girardi used this value)
+MBOL_SUN = 4.77 # (from Bahcall et al 1995, Girardi used this value)
+LOG_G = 4.5 # for M37
+DISTANCE = 1490. # In parsec for M37
+DISTANCE_SD = 120. # In parsec for M37
+EXTINCTIONr = 0.518695 # A_r from Schlafly11 tables (Rv=3.1, E(B-V)=0.227; Av / E(B-V)=2.285)
+EXTINCTIONg = 0.749781 # from Schlafly11 tables  (Rv=3.1, E(B-V)=0.227)
+EXTINCTIONi = 0.385446 # from Schlafly11 tables  (Rv=3.1, E(B-V)=0.227)
+pc_to_cm = 3.08567758e18
