9c8c92abea3ae8196096d25c44f43231a8825b6e
stephtdouglas <stephanietdouglas@gmail.com>
2015-06-23 18:47:06 -0400

    Calculate Lx and LxLbol

--- luminosities/lx.py
+++ luminosities/lx.py
@@ -6,23 +6,30 @@ import matplotlib.pyplot as plt
 import astropy.io.ascii as at
 
 
-# ROSAT =======================================================================
-# Calculate Lx & Lx_sd ------------------------------------
-# flux is in erg s^-1 cm^-2; DISTANCE is in pc
-Lx = 4 * np.pi * xfluxr * (DISTANCE * pc_to_cm)**2
-parderfX = 4 * np.pi * (DISTANCE * pc_to_cm)**2 # partial deriv. wrt f_X
-parderD = 8 * np.pi * (DISTANCE * pc_to_cm) * xfluxr
-Lx_sd = np.sqrt(parderfX**2 * xfluxr_sd**2 + parderD**2 * (DISTANCE_SD * pc_to_cm)**2)
+def calc_lx_rosat(xflux,distance=DISTANCE,unc_distance=distance_SD,
+    null_val=-9999.):
+    """Calculate Lx & unc_Lx. Flux is in erg s^-1 cm^-2; distance is in pc."""
+    Lx = 4 * np.pi * xflux * (distance * pc_to_cm)**2
+    Lx[xflux=null_val] = null_val
 
-# Calculate LL & LL_sd ------------------------------------
-LL = np.zeros(num)
-LL_sd = np.zeros(num)
-LL[irange] = Lx[irange] / Lbol[irange]
-LL_sd[irange] = np.sqrt(Lbol[irange]**-2 * Lx_sd[irange]**2 \
-                         + Lx[irange]**2 * Lbol[irange]**-4 * Lbol_sd[irange]**2)
+    # Propagate uncertainties to get uncertainty on Lx
+    parderfX = 4 * np.pi * (distance * pc_to_cm)**2 # partial deriv. wrt f_X
+    parderD = 8 * np.pi * (distance * pc_to_cm) * xfluxr
+    unc_Lx = np.sqrt(parderfX**2 * xfluxr_sd**2 + 
+                     parderD**2 * (distance_SD * pc_to_cm)**2)
+    unc_Lx[xflux=null_val] = null_val
 
-outdata[:,7] = Lx
-outdata[:,8] = Lx_sd
-outdata[:,9] = LL
-outdata[:,10] = LL_sd
+    return Lx, unc_Lx
 
+def calc_lx_lbol(Lx,unc_Lx,Lbol,unc_Lbol,null_val=-9999):
+    """Calculate Lx/Lbol (LL) and uncertainty (unc_LL)."""
+
+    # Calculate the luminosity ratio
+    LL = Lx / Lbol
+    LL[(Lx==null_val) | (Lbol==null_val)] = null_val
+
+    # Propagate uncertainties to get uncertainty on Lx/Lbol
+    unc_LL = np.sqrt(Lbol**-2 * Lx_sd**2 + Lx**2 * Lbol**-4 * Lbol_sd**2)
+    unc_LL[(Lx==null_val) | (Lbol==null_val)] = null_val
+
+    return LL, unc_LL
