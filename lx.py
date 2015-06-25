fixing unc_Lx calculation

lx.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import astropy.io.ascii as at
 
 
-def calc_lx_rosat(xflux,distance=DISTANCE,unc_distance=distance_SD,
+def calc_lx_rosat(xflux,unc_xflux,distance=DISTANCE,unc_distance=distance_SD,
     null_val=-9999.):
     """Calculate Lx & unc_Lx. Flux is in erg s^-1 cm^-2; distance is in pc."""
     Lx = 4 * np.pi * xflux * (distance * pc_to_cm)**2
@@ -14,9 +14,9 @@ def calc_lx_rosat(xflux,distance=DISTANCE,unc_distance=distance_SD,
 
     # Propagate uncertainties to get uncertainty on Lx
     parderfX = 4 * np.pi * (distance * pc_to_cm)**2 # partial deriv. wrt f_X
-    parderD = 8 * np.pi * (distance * pc_to_cm) * xfluxr
-    unc_Lx = np.sqrt(parderfX**2 * xfluxr_sd**2 + 
-                     parderD**2 * (distance_SD * pc_to_cm)**2)
+    parderD = 8 * np.pi * (distance * pc_to_cm) * xflux
+    unc_Lx = np.sqrt(parderfX**2 * unc_xflux**2 + 
+                     parderD**2 * (unc_distance * pc_to_cm)**2)
     unc_Lx[xflux=null_val] = null_val
 
     return Lx, unc_Lx